3d8a0ea8446c8c0a7833a90bcfba5af5d1c3e390
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-04-25 13:37:48 -0300

    art red

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,9 +9,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x -= speed
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
-	
-	if Input.is_action_pressed("ui_up"):
-		velocity.y += speed
-	if Input.is_action_pressed("ui_down"):
-		velocity.y -= speed 
+
 	move_and_collide(velocity * delta)
